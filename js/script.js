747806ea98b093455d82ed17f911c31203e49491
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-03-20 17:12:47 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Shanea Jaromay All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Shanea Jaromay
+// Created on: March 2025
 // This file contains the JS functions for index.html
 
+"use strict"
+
 /**
- * Displays an alert with the message "Hello, World!"
+ * This function calculates area of a triangle.
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function calculateAreaOfTriangle () {
+  // input
+  const baseOfTriangle = parseInt(document.getElementById('base-of-triangle').value)
+  const heightOfTriangle = parseInt(document.getElementById('height-of-triangle').value)
+
+  // process
+  const areaOfTriangle = (baseOfTriangle * heightOfTriangle) / 2
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + areaOfTriangle + ' cm²'
 }
